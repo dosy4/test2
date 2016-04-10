a5e4a32246d4b07f5aaf62df0de91082ae28c846
dosy4 <dosedelpet@gmail.com>
2016-04-10 21:11:00 +0200

    přidal jsem pico

--- main.c
+++ main.c
@@ -36,7 +36,7 @@ printf("number: %li (prime)\n", cislo);
 return 0;
 }
 
-int palindrom (char *retezec) // funkce urcujici zda neni slovo palindrom pico
+int palindrom (char *retezec) // funkce urcujici zda neni slovo palindrom pico pico z netu
 {
 int pocetpismen=0;
 int zacatek;
